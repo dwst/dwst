f14b80edc13f7f7287bbeea4e2d20a9c59a8b086
Toni Ruottu <toni.ruottu@iki.fi>
2016-06-09 23:25:09 +0300

    Add build and clean tasks to gulp

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,8 @@ const gulp = require('gulp');
 const jsonlint = require('gulp-jsonlint');
 const eslint = require('gulp-eslint');
 const htmlhint = require('gulp-htmlhint');
+const zip = require('gulp-zip');
+const clean = require('gulp-clean');
 
 gulp.task('jsonlint', () => {
   return gulp.src(['**/*.json', '!node_modules/**'])
@@ -29,3 +31,15 @@ gulp.task('validate', ['jsonlint', 'eslint', 'htmlhint']);
 
 gulp.task('test', ['validate']);
 
+gulp.task('clean', () => {
+  return gulp.src('build', {read: false})
+    .pipe(clean());
+});
+
+gulp.task('build', ['clean'], () => {
+  return gulp.src('dwst/*')
+    .pipe(zip('dwst.zip'))
+    .pipe(gulp.dest('build'));
+});
+
+gulp.task('default', ['build']);
