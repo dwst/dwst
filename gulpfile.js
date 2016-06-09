ce212cae9d9dfee0d94e9a99890705605cb8f446
Toni Ruottu <toni.ruottu@iki.fi>
2016-06-09 22:17:31 +0300

    Add eslint gulp task

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 var gulp = require('gulp');
 var jsonlint = require('gulp-jsonlint');
+var eslint = require('gulp-eslint');
 
 gulp.task('jsonlint', () => {
   gulp.src('**/*.json')
@@ -7,7 +8,14 @@ gulp.task('jsonlint', () => {
     .pipe(jsonlint.failOnError())
 });
 
-gulp.task('validate', ['jsonlint']);
+gulp.task('eslint', () => {
+    return gulp.src(['**/*.js','!node_modules/**'])
+        .pipe(eslint())
+        .pipe(eslint.format())
+        .pipe(eslint.failAfterError());
+});
+
+gulp.task('validate', ['jsonlint', 'eslint']);
 
 gulp.task('test', ['validate']);
 
