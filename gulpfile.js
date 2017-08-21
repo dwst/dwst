6710d6814d236a1f8201eb6f8c83767190a337a3
Toni Ruottu <toni.ruottu@iki.fi>
2017-08-21 15:44:49 +0300

    Add build tasks to gulp

--- gulpfile.js
+++ gulpfile.js
@@ -3,10 +3,17 @@
 /* global require */
 
 const gulp = require('gulp');
+const gulpSequence = require('gulp-sequence');
 const jsonlint = require('gulp-jsonlint');
 const eslint = require('gulp-eslint');
 const htmlhint = require('gulp-htmlhint');
 const browserSync = require('browser-sync').create();
+const clean = require('gulp-clean');
+const webpackStream = require('webpack-stream');
+const webpack2 = require('webpack');
+const fse = require('fs-extra');
+const postcss = require('gulp-postcss');
+const atImport = require('postcss-import');
 
 gulp.task('jsonlint', () => {
   return gulp.src(['**/*.json', '.htmlhintrc', '!node_modules/**'])
@@ -15,14 +22,14 @@ gulp.task('jsonlint', () => {
 });
 
 gulp.task('eslint', () => {
-  return gulp.src(['**/*.js', '!node_modules/**'])
+  return gulp.src(['**/*.js', '!node_modules/**', '!build/**'])
     .pipe(eslint())
     .pipe(eslint.format())
     .pipe(eslint.failAfterError());
 });
 
 gulp.task('htmlhint', () => {
-  gulp.src(['**/*.html', '!node_modules/**'])
+  gulp.src(['**/*.html', '!node_modules/**', '!build/**'])
     .pipe(htmlhint('.htmlhintrc'))
     .pipe(htmlhint.failReporter());
 });
@@ -30,8 +37,6 @@ gulp.task('htmlhint', () => {
 gulp.task('validate', ['jsonlint', 'eslint', 'htmlhint']);
 
 gulp.task('test', ['validate']);
-
-gulp.task('default');
 
 gulp.task('sync-css', () => {
   return gulp.src(['dwst/**/*.css'])
@@ -50,3 +55,51 @@ gulp.task('browser-sync', () => {
 });
 
 gulp.task('dev', ['browser-sync']);
+
+gulp.task('clean', () => {
+  return gulp.src('build/', {read: false})
+    .pipe(clean());
+});
+
+gulp.task('build-css', () => {
+  return gulp.src('dwst/dwst.css')
+    .pipe(postcss([
+      atImport(),
+    ]))
+    .pipe(gulp.dest('build/'));
+});
+
+gulp.task('build-js', () => {
+  return gulp.src('dwst/dwst.js')
+    .pipe(webpackStream({
+      output: {
+        filename: 'dwst.js',
+      },
+    }, webpack2))
+    .pipe(gulp.dest('build/'));
+});
+
+gulp.task('build-html', () => {
+  return gulp.src('dwst/dwst.html')
+    .pipe(gulp.dest('build/'));
+});
+
+gulp.task('build-images', () => {
+  return gulp.src(['dwst/**/*.png', 'dwst/**/*.ico'])
+    .pipe(gulp.dest('build/'));
+});
+
+gulp.task('build-manifest', () => {
+  return gulp.src('dwst/manifest.json')
+    .pipe(gulp.dest('build/'));
+});
+
+gulp.task('build-assets', ['build-js', 'build-css', 'build-html', 'build-images', 'build-manifest']);
+
+gulp.task('create-symlinks', () => {
+  fse.ensureLinkSync('build/dwst.html', 'build/index.html');
+});
+
+gulp.task('build', gulpSequence('clean', 'build-assets', 'create-symlinks'));
+
+gulp.task('default', ['build']);
