f554a4d2fc0893097db45c8278176d994bba1fe0
Toni Ruottu <toni.ruottu@iki.fi>
2017-08-25 11:59:35 +0300

    Clean up copying information

--- dwst/scripts/terminal.js
+++ dwst/scripts/terminal.js
@@ -1,4 +1,17 @@
 
+/**
+
+  Authors: Toni Ruottu, Finland 2010-2017
+
+  This file is part of Dark WebSocket Terminal.
+
+  CC0 1.0 Universal, http://creativecommons.org/publicdomain/zero/1.0/
+
+  To the extent possible under law, Dark WebSocket Terminal developers have waived all
+  copyright and related or neighboring rights to Dark WebSocket Terminal.
+
+*/
+
 import utils from './utils.js';
 import currenttime from './currenttime.js';
 
