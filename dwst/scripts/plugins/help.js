7aa6f9a8c59cda428034440e58a6a79149287dd9
Toni Ruottu <toni.ruottu@iki.fi>
2017-11-27 18:26:28 +0200

    Document basic usage

--- dwst/scripts/plugins/help.js
+++ dwst/scripts/plugins/help.js
@@ -112,6 +112,15 @@ export default class Help {
       },
       '',
       [
+        '- ',
+        {
+          type: 'dwstgg',
+          text: '#introduction',
+          section: '#introduction',
+        },
+        ' for beginners',
+      ],
+      [
         '- Working with ',
         {
           type: 'dwstgg',
@@ -415,6 +424,99 @@ export default class Help {
         futureChanges,
         '',
       ]), 'system');
+      return;
+    }
+    if (page === '#introduction') {
+      this._dwst.terminal.mlog([
+        this._createBreadCrumbs(page),
+        '',
+        {
+          type: 'h1',
+          text: 'Introduction for Beginners',
+        },
+        '',
+        'DWST is used to manually interact with a WebSocket server.',
+        '',
+        {
+          type: 'h2',
+          text: 'The Very Basics',
+        },
+        '',
+        [
+          'Use the ',
+          {
+            type: 'dwstgg',
+            text: 'connect',
+            section: 'connect',
+          },
+          ' command to establish a connection. ',
+          'Type in text to send messages. ',
+          'End the connection with the ',
+          {
+            type: 'dwstgg',
+            text: 'disconnect',
+            section: 'disconnect',
+          },
+          ' command when you are done.',
+        ],
+        '',
+        {
+          type: 'h2',
+          text: 'Convenience Tools',
+        },
+        '',
+        [
+          'Use the ',
+          {
+            type: 'dwstgg',
+            text: 'send',
+            section: 'send',
+          },
+          ' and ',
+          {
+            type: 'dwstgg',
+            text: 'binary',
+            section: 'binary',
+          },
+          ' commands to construct more complex messages. ',
+          'Setup a periodic send with the ',
+          {
+            type: 'dwstgg',
+            text: 'interval',
+            section: 'interval',
+          },
+          ' command or send a burst of messages with the ',
+          {
+            type: 'dwstgg',
+            text: 'spam',
+            section: 'spam',
+          },
+          ' command.',
+        ],
+        '',
+        {
+          type: 'h2',
+          text: 'In Case of Emergency',
+        },
+        '',
+        [
+          'Use the ',
+          {
+            type: 'strong',
+            text: 'escape key',
+          },
+          ' for an emergency shutdown if you feel that things are spinning out of control. ',
+          '',
+          'Click on the DWST logo or use the ',
+          {
+            type: 'dwstgg',
+            text: 'splash',
+            section: 'splash',
+          },
+          ' command if you get lost.',
+        ],
+        '',
+      ], 'system');
       return;
     }
 
