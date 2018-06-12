e0290f110c37bcc973352288c68fefad870089f7
Toni Ruottu <toni.ruottu@iki.fi>
2018-06-12 13:30:32 +0300

    increase version number to 2.4.9

--- dwst/scripts/dwst.js
+++ dwst/scripts/dwst.js
@@ -132,7 +132,7 @@ const terminal = new Terminal('ter1', controller);
 
 const pluginInterface = {
 
-  VERSION: '2.4.8',
+  VERSION: '2.4.9',
   ECHO_SERVER_URL: 'wss://echo.websocket.org/',
 
   terminal,
